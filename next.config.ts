2df1e2bc209184a5b3d30d215057baff26fa5299
crrashh1542 <1542@crrashh.com>
2026-04-27 13:50:55 +0800

    release: v0.1.1
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next'
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ['10.106.224.246'],
-    // assetPrefix: isProd ? 'https://obj.crrashh.com/prod/assets_sleepify' : undefined
+    allowedDevOrigins: ['10.106.*.*'],
+    assetPrefix: isProd ? 'https://obj.crrashh.com/prod/assets_sleepify' : undefined,
 }
 
 export default nextConfig
